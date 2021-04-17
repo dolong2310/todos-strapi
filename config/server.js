672bb6ee413595b1987d54fae126dffc12c941f6
long do <dolong2310@gmail.com>
2021-04-18 00:25:25 +0700

    add google auth

--- config/server.js
+++ config/server.js
@@ -1,9 +1,10 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  url: "https://c554b4d4dbf5.ngrok.io",
   admin: {
     auth: {
-      secret: env('ADMIN_JWT_SECRET', '7f7fcac5fe688c485ddab7de83053b37'),
+      secret: env("ADMIN_JWT_SECRET", "7f7fcac5fe688c485ddab7de83053b37"),
     },
   },
 });
