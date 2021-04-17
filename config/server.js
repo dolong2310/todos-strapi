8f10184ac752bbc6625bc198d28df5096401b679
long do <dolong2310@gmail.com>
2021-04-18 01:14:28 +0700

    change

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://c554b4d4dbf5.ngrok.io",
+  // url: 'https://c554b4d4dbf5.ngrok.io',
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "7f7fcac5fe688c485ddab7de83053b37"),
